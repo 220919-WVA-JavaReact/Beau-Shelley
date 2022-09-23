67788052371ef39acab44627f0aed05ebb026a16
BeauShelley <91709282+BeauShelley@users.noreply.github.com>
2022-09-23 08:09:07 -0700

    organizing files

---
+++ assignments/week 1/22SEP2022/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+fname=${1}
+lname=${2}
+email=${3}
+uname=${4}
+pw=${5}
+
+read -p "First Name: " $fname;
+#read -p "Last Name: " $lname;
+#read -p "Email: " $email;
+#read -p "User Name: " $uname;
+#read -p "Password: " $pw;
+echo $fname
+
+#echo $fname, $lname >> names.csv
